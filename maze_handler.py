387e6b70f3a7e121ac481502fe35fa9bea55b058
frinfo702 <121755762+frinfo702@users.noreply.github.com>
2025-04-15 15:53:38 +0900

    make_reversed_maze関数内での配列初期化を改善し、numpyのzeros関数を使用してコードを簡素化。

--- maze_handler.py
+++ maze_handler.py
@@ -4,7 +4,7 @@ import numpy as np
 def make_reversed_maze(maze: np.ndarray) -> np.ndarray:
     height, width = maze.shape
 
-    reversed_maze = np.array([[0 for _ in range(height)] for _ in range(width)])
+    reversed_maze = np.zeros(maze.shape, dtype=int)
     # 上下反転
     for row in range(height):
         for col in range(width):
